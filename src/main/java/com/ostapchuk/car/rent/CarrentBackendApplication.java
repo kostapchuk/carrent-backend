103bc58117c54697bd2b8eeab67c336e309f45a3
k.ostapchuk <xfaseex1@gmail.com>
2022-03-17 15:56:03 +0300

    feat: add main

---
+++ src/main/java/com/ostapchuk/car/rent/CarrentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ostapchuk.car.rent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrentBackendApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(CarrentBackendApplication.class, args);
+    }
+
+}
