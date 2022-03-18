b2cefe33658a1e8e627c5616afb10ba7fd0af54b
k.ostapchuk <xfaseex1@gmail.com>
2022-03-18 09:51:23 +0300

    feat: add new handlers

--- src/main/java/com/ostapchuk/car/rent/exception/ExceptionAdviser.java
+++ src/main/java/com/ostapchuk/car/rent/exception/ExceptionAdviser.java
@@ -1,11 +1,14 @@
 package com.ostapchuk.car.rent.exception;
 
 import com.ostapchuk.car.rent.dto.ErrorResponseDto;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 @RestControllerAdvice
 public record ExceptionAdviser() {
@@ -15,5 +18,17 @@ public record ExceptionAdviser() {
             UserUnverifiedException.class})
     public ErrorResponseDto entityNotFoundException(final RuntimeException ex) {
         return new ErrorResponseDto(ex.getMessage(), BAD_REQUEST.value());
+    }
+
+    @ResponseStatus(FORBIDDEN)
+    @ExceptionHandler(AccessDeniedException.class)
+    public ErrorResponseDto accessDeniedException(final AccessDeniedException ex) {
+        return new ErrorResponseDto(ex.getMessage(), FORBIDDEN.value());
+    }
+
+    @ResponseStatus(UNAUTHORIZED)
+    @ExceptionHandler(JwtAuthenticationException.class)
+    public ErrorResponseDto jwtAuthenticationException(final AccessDeniedException ex) {
+        return new ErrorResponseDto(ex.getMessage(), UNAUTHORIZED.value());
     }
 }
