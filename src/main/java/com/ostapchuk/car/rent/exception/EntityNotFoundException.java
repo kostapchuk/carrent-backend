3612f6d8822f8ee52c75e2c3fe33948751808e50
k.ostapchuk <xfaseex1@gmail.com>
2022-03-18 09:51:03 +0300

    refactor: code cleanup

--- src/main/java/com/ostapchuk/car/rent/exception/EntityNotFoundException.java
+++ src/main/java/com/ostapchuk/car/rent/exception/EntityNotFoundException.java
@@ -2,15 +2,7 @@ package com.ostapchuk.car.rent.exception;
 
 public class EntityNotFoundException extends RuntimeException {
 
-    public EntityNotFoundException() {
-        super();
-    }
-
     public EntityNotFoundException(final String message) {
         super(message);
-    }
-
-    public EntityNotFoundException(final String message, final Throwable cause) {
-        super(message, cause);
     }
 }
--- src/main/java/com/ostapchuk/car/rent/exception/NegativeBalanceException.java
+++ src/main/java/com/ostapchuk/car/rent/exception/NegativeBalanceException.java
@@ -2,9 +2,6 @@ package com.ostapchuk.car.rent.exception;
 
 public class NegativeBalanceException extends RuntimeException {
 
-    public NegativeBalanceException() {
-    }
-
     public NegativeBalanceException(final String message) {
         super(message);
     }
