79c1fafe0c135ea1743edeffc53a48a6e665d193
k.ostapchuk <xfaseex1@gmail.com>
2022-03-18 09:51:41 +0300

    feat: add security service

---
+++ src/main/java/com/ostapchuk/car/rent/security/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.ostapchuk.car.rent.security;
+
+import com.ostapchuk.car.rent.entity.User;
+import com.ostapchuk.car.rent.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(final String email) throws UsernameNotFoundException {
+        final User user = userRepository.findByEmail(email).orElseThrow(() ->
+                new UsernameNotFoundException("User does not exist"));
+        return SecurityUser.fromUser(user);
+    }
+}
