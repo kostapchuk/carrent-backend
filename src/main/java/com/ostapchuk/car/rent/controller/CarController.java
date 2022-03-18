b1f980effd4b5a580c811bb80def336392f4f336
k.ostapchuk <xfaseex1@gmail.com>
2022-03-18 09:49:52 +0300

    feat: add permissions

--- src/main/java/com/ostapchuk/car/rent/controller/CarController.java
+++ src/main/java/com/ostapchuk/car/rent/controller/CarController.java
@@ -3,6 +3,8 @@ package com.ostapchuk.car.rent.controller;
 import com.ostapchuk.car.rent.dto.CarDto;
 import com.ostapchuk.car.rent.dto.CarsDto;
 import com.ostapchuk.car.rent.service.CarService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,9 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/cars")
-public record CarController(CarService carService) {
+@RequiredArgsConstructor
+public class CarController {
+
+    private final CarService carService;
 
     @PostMapping
+    @PreAuthorize("hasAuthority('users:write')")
     public void save(@RequestBody final CarDto carDto) {
         carService.save(carDto);
     }
@@ -27,16 +33,19 @@ public record CarController(CarService carService) {
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('users:read')")
     public CarDto findById(@PathVariable final Integer id) {
         return carService.findDtoById(id);
     }
 
     @PutMapping
+    @PreAuthorize("hasAuthority('users:write')")
     public void update(@RequestBody final CarDto carDto) {
         carService.update(carDto);
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('users:delete')")
     public void delete(@PathVariable final Integer id) {
         carService.delete(id);
     }
--- src/main/java/com/ostapchuk/car/rent/controller/OrderController.java
+++ src/main/java/com/ostapchuk/car/rent/controller/OrderController.java
@@ -2,6 +2,8 @@ package com.ostapchuk.car.rent.controller;
 
 import com.ostapchuk.car.rent.dto.OrderDto;
 import com.ostapchuk.car.rent.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/orders")
-public record OrderController(OrderService orderService) {
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
 
     @PostMapping
+    @PreAuthorize("hasAuthority('users:read')")
     public void save(@RequestBody final OrderDto orderDto) {
         orderService.process(orderDto);
     }
