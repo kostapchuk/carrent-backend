2ecf6170b41e9e39065b6a8d5f9b865e292bf7c8
k.ostapchuk <xfaseex1@gmail.com>
2022-03-18 09:50:20 +0300

    feat: enable auto config props

--- src/main/java/com/ostapchuk/car/rent/CarrentBackendApplication.java
+++ src/main/java/com/ostapchuk/car/rent/CarrentBackendApplication.java
@@ -1,9 +1,12 @@
 package com.ostapchuk.car.rent;
 
+import com.ostapchuk.car.rent.config.JwtConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({JwtConfig.class})
 public class CarrentBackendApplication {
 
     public static void main(final String[] args) {
