d572a25c238c567d992ce73a226fdd81106183a3
k.ostapchuk <xfaseex1@gmail.com>
2022-04-28 21:33:19 +0300

    feat: split project into multimodule project

---
+++ email-service/src/main/java/com/ostapchuk/email/service/EmailServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ostapchuk.email.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailServiceApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(EmailServiceApplication.class, args);
+    }
+
+}
