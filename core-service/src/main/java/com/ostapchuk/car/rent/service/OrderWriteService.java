b5e6629462de33ee82d5dd49e30c06f4d9269032
kir-ostapchuk <xfaseex1@gmail.com>
2022-11-09 13:00:28 +0300

    refactor: remove cycle dependencies, remove aws implementation, upd env vars

--- core-service/src/main/java/com/ostapchuk/car/rent/service/OrderWriteService.java
+++ core-service/src/main/java/com/ostapchuk/car/rent/service/OrderWriteService.java
@@ -1,8 +1,6 @@
 package com.ostapchuk.car.rent.service;
 
-import com.ostapchuk.car.rent.dto.order.OrderDto;
 import com.ostapchuk.car.rent.entity.Order;
-import com.ostapchuk.car.rent.processor.StartingRideStatusProcessor;
 import com.ostapchuk.car.rent.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,12 +12,7 @@ import javax.transaction.Transactional;
 @RequiredArgsConstructor
 public class OrderWriteService {
 
-    private final StartingRideStatusProcessor startingRideStatusProcessor;
     private final OrderRepository orderRepository;
-
-    public void process(final OrderDto orderDto) {
-        startingRideStatusProcessor.process(orderDto);
-    }
 
     public Order save(final Order order) {
         return orderRepository.save(order);
